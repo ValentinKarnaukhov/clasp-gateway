a6fab51efab19e01847505abc56e5fec85745a0f
Valentin <valentine.karnaukhov@gmail.com>
2020-02-23 20:48:50 +0300

    Init commint

---
+++ src/main/java/com/elmistsoft/claspgateway/ClaspGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.elmistsoft.claspgateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClaspGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClaspGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/elmistsoft/claspgateway/ClaspGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.elmistsoft.claspgateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClaspGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
